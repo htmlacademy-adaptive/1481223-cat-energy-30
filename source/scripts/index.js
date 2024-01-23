d17e495206c6c7481fb02cce10d4cfdc28c53c48
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-01-23 18:35:26 +0300

    Доработки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+document.querySelector('.main-header__container--no-js').classList.remove('main-header__container--no-js');
+const buttonToggle = document.querySelector('.button-toggle--no-js');
+buttonToggle.classList.remove('button-toggle--no-js');
+const headerNav = document.querySelector('.main-header__navigation--no-js');
+headerNav.classList.remove('main-header__navigation--no-js');
+buttonToggle.addEventListener('click', () => {
+  buttonToggle.classList.toggle('button-toggle--close');
+  headerNav.classList.toggle('main-header__navigation--show');
+});
