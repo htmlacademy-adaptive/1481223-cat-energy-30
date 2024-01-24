8623d991e8602a694335596ffe81287055f55645
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-01-24 11:19:16 +0300

    Сделал карту при отключенном js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,6 @@
 /* в этот файл добавляет скрипты*/
 document.querySelector('.main-header__container--no-js').classList.remove('main-header__container--no-js');
+document.querySelector('.main-footer__iframe-image--no-js').classList.remove('main-footer__iframe-image--no-js');
 const buttonToggle = document.querySelector('.button-toggle--no-js');
 buttonToggle.classList.remove('button-toggle--no-js');
 const headerNav = document.querySelector('.main-header__navigation--no-js');
